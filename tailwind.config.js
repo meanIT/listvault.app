bd08581a289d9c6affb3b1b0a406bcc7fe5cb88d
Valeri Karpov <val@karpov.io>
2024-07-03 17:03:00 -0400

    getting started

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './public/*.html'
+  ],
+  corePlugins: {
+    container: true 
+  },
+  plugins: [
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'picton-blue': {
+          '50': '#f2f8fd',
+          '100': '#e3f0fb',
+          '200': '#c2e1f5',
+          '300': '#8cc8ed',
+          '400': '#62b5e5',
+          '500': '#2791d0',
+          '600': '#1974b0',
+          '700': '#155d8f',
+          '800': '#164f76',
+          '900': '#174363',
+          '950': '#102a41',
+        }
+      }
+    }
+  }
+};
